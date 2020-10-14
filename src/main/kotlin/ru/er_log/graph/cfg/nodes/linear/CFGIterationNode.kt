4bb0cdbc11eecbc1ed327162d5478c87bcf1fec4
Eldar <eldar.tim@gmail.com>
2020-10-14 18:36:23 +0300

    Cycles fix
    Циклы теперь соединяются с другими вершинами корректно.

--- src/main/kotlin/ru/er_log/graph/cfg/nodes/linear/CFGIterationNode.kt
+++ src/main/kotlin/ru/er_log/graph/cfg/nodes/linear/CFGIterationNode.kt
@@ -3,6 +3,8 @@ package ru.er_log.graph.cfg.nodes.linear
 import ru.er_log.graph.StyleCatalogue
 import ru.er_log.graph.cfg.nodes.CFGBodyNode
 import ru.er_log.graph.cfg.nodes.CFGLink
+import ru.er_log.graph.cfg.nodes.CFGNode
+import java.util.*
 
 abstract class CFGIterationNode(context: Int, deepness: Int, title: String = "iteration statement") :
     CFGBodyNode(context, deepness, title, StyleCatalogue.NodeStyles.iteration)
@@ -11,15 +13,42 @@ abstract class CFGIterationNode(context: Int, deepness: Int, title: String = "it
         nodesForLinking().forEach { it.link(body.first(), CFGLink.LinkType.DIR_BACK) }
     }
 
-//    override fun leaves(): MutableSet<CFGNode> {
-//        return mutableSetOf()
-//    }
-//
-//    override fun nodesForLinking(): MutableSet<CFGNode> {
-//        val nodes = super.nodesForLinking()
-//        nodes.add(this)
-//        return nodes
-//    }
+    /** Смотреть описание [leaves]. */
+    private var wasLinked: Boolean = false
+
+    /**
+     * Поскольку циклы имеют, как правило, цикловидную структуру графа, для того чтобы
+     * соединить цикл со следующим элементом внутри одного блока, необходимо вручную
+     * отслеживать, является ли цикл уже соединенным с чем-либо еще, помимо своего тела.
+     */
+    override fun leaves(): MutableSet<CFGNode> {
+        val leaves = super.leaves()
+        if (!wasLinked && leaves.isEmpty() && haveLoop()) {
+            leaves.add(this)
+            wasLinked = true
+        }
+        return leaves
+    }
+
+    /**
+     * Рекурсивный поиск данного узла во всех подузлах [CFGNode.links].
+     *
+     * @param entry     вершина начала поиска
+     * @param visited   список посещенных вершин
+     * @return true, если совпадение найдено
+     */
+    private fun haveLoop(entry: CFGNode = this, visited: List<CFGNode> = mutableListOf(entry)): Boolean {
+        entry.links.forEach { link ->
+            if (link.to == this) { return true }
+
+            val nodeVisited = visited.toMutableList()
+            if (nodeVisited.contains(link.to)) { return@forEach }
+            else { nodeVisited.add(link.to) }
+
+            if (haveLoop(link.to, nodeVisited)) { return true }
+        }
+        return false
+    }
 }
 
 data class CFGNodeForStatement(
